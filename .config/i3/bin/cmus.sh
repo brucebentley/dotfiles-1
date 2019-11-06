8d0a40d71b273d45897868e448c90ebe69e30034
Emanon <emanon@localhost.localdomain>
2019-11-05 20:47:05 -0500

    Change cmus script

--- .config/i3/bin/cmus.sh
+++ .config/i3/bin/cmus.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
-stat=$(cmus-remote -Q 2> /dev/null | rg status | cut -d ' ' -f2-)
-duration=$(cmus-remote -Q 2> /dev/null | rg duration | cut -d ' ' -f2-)
-current=$(cmus-remote -Q 2> /dev/null | rg position | cut -d ' ' -f2-)
-artist=$(cmus-remote -Q 2> /dev/null | rg ' artist ' | cut -d ' ' -f3-)
-song=$(cmus-remote -Q 2> /dev/null | rg title | cut -d ' ' -f3-)
+#!/bin/bash
+stat=$(cmus-remote -Q 2> /dev/null | ag status | cut -d ' ' -f2-)
+duration=$(cmus-remote -Q 2> /dev/null | ag duration | cut -d ' ' -f2-)
+current=$(cmus-remote -Q 2> /dev/null | ag position | cut -d ' ' -f2-)
+artist=$(cmus-remote -Q 2> /dev/null | ag ' artist ' | cut -d ' ' -f3-)
+song=$(cmus-remote -Q 2> /dev/null | ag title | cut -d ' ' -f3-)
 echo "$artist - $song $current / $duration "
