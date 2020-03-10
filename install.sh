65f323e8568709dc0355b0323c4fd5375b978190
khue <khuenguyrn123456@gmail.com>
2020-03-10 03:46:48 -0400

    make all symlink relative

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # SIMPLE BASH INSTALLATION SCRIPT (TOO LAZY TO SETUP ANSIBLE)
 
 # HOME DIRECTORY
-ln -sf  $PWD/.asoundrc  $HOME
+ln -sfr $PWD/.asoundrc  $HOME
 ln -sfr $PWD/.fonts     $HOME
 ln -sfr $PWD/.mpd       $HOME
 ln -sfr $PWD/.ncmpcpp   $HOME
-ln -sf  $PWD/.tmux.conf $HOME
-ln -sf  $PWD/.xinitrc   $HOME
-ln -sf  $PWD/.z*        $HOME
+ln -sfr $PWD/.tmux.conf $HOME
+ln -sfr $PWD/.xinitrc   $HOME
+ln -sfr $PWD/.z*        $HOME
 ln -sfr $PWD/.zsh       $HOME
 
 # ~/.config
