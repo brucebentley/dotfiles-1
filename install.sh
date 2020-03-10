c9930b22eda30283a5361e48b2363e67039d1d2f
khue <khuenguyrn123456@gmail.com>
2020-03-10 03:26:19 -0400

    fix spacing

--- install.sh
+++ install.sh
@@ -3,30 +3,37 @@
 # SIMPLE BASH INSTALLATION SCRIPT (TOO LAZY TO SETUP ANSIBLE)
 
 # HOME DIRECTORY
-ln -sf $PWD/.asoundrc $HOME
-ln -sfr $PWD/.fonts $HOME
-ln -sfr $PWD/.mpd $HOME
-ln -sfr $PWD/.ncmpcpp $HOME
-ln -sf $PWD/.tmux.conf $HOME
-ln -sfr $PWD/.vim $HOME
-ln -sf $PWD/.xinitrc $HOME
-ln -sf $PWD/.z* $HOME
-ln -sfr $PWD/.zsh $HOME
+ln -sf  $PWD/.asoundrc  $HOME
+ln -sfr $PWD/.fonts     $HOME
+ln -sfr $PWD/.mpd       $HOME
+ln -sfr $PWD/.ncmpcpp   $HOME
+ln -sf  $PWD/.tmux.conf $HOME
+ln -sf  $PWD/.xinitrc   $HOME
+ln -sf  $PWD/.z*        $HOME
+ln -sfr $PWD/.zsh       $HOME
 
 # ~/.config
-mkdir $HOME/.config
+if [ ! -d $HOME/.config ]; then
+  mkdir $HOME/.config
+fi
 
 ln -sfr $PWD/.config/* $HOME/.config/
 
+# create symlink to .vim if target path doesn't exist
 if [ ! -L $HOME/.config/vim ]; then
   ln -sfr $PWD/.vim $HOME/.config/nvim
+  ln -sfr $PWD/.vim $HOME
 fi
 
 # Setup Command-T
-cd $HOME/.vim/pack/bundle/opt/Command-T/ruby/command-t/ext/command-t/
-ruby extconf.rb
-make
+if [ ! -f $HOME/.vim/pack/bundle/opt/Command-T/ruby/command-t/ext/command-t/ext.so ]; then
+  cd $HOME/.vim/pack/bundle/opt/Command-T/ruby/command-t/ext/command-t/
+  ruby extconf.rb
+  make
+fi
 
 # Install skim
-cd $HOME/.zsh/vendor/skim
-bash install
+if [ ! -f $HOME/.zsh/vendor/skim/bin/sk ]; then
+  cd $HOME/.zsh/vendor/skim
+  bash install
+fi
