f678f5fa7c5297cf421bf9af5b5ce3cc574c18b9
khue <khuenguyrn123456@gmail.com>
2020-03-18 02:21:11 -0400

    (install.sh)disable relative location symlink

--- install.sh
+++ install.sh
@@ -3,28 +3,28 @@
 # SIMPLE BASH INSTALLATION SCRIPT (TOO LAZY TO SETUP ANSIBLE)
 
 # HOME DIRECTORY
-ln -sfr $PWD/.asoundrc  $HOME
-ln -sfr $PWD/.fonts     $HOME
-ln -sfr $PWD/.inputrc   $HOME
-ln -sfr $PWD/.mpd       $HOME
-ln -sfr $PWD/.ncmpcpp   $HOME
-ln -sfr $PWD/.tmux.conf $HOME
-ln -sfr $PWD/.xinitrc   $HOME
-ln -sfr $PWD/.z*        $HOME
-ln -sfr $PWD/.zsh       $HOME
+ln -sf $PWD/.asoundrc  $HOME
+ln -sf $PWD/.fonts     $HOME
+ln -sf $PWD/.inputrc   $HOME
+ln -sf $PWD/.mpd       $HOME
+ln -sf $PWD/.ncmpcpp   $HOME
+ln -sf $PWD/.tmux.conf $HOME
+ln -sf $PWD/.xinitrc   $HOME
+ln -sf $PWD/.z*        $HOME
+ln -sf $PWD/.zsh       $HOME
 
 # ~/.config
 if [ ! -d $HOME/.config ]; then
   mkdir $HOME/.config
 fi
 
-ln -sfr $PWD/.config/* $HOME/.config/
+ln -sf $PWD/.config/* $HOME/.config/
 
 # create symlink to .vim if target path doesn't exist
 if [ ! -d $HOME/.config/nvim ]; then
-  ln -sfr $PWD/.vim $HOME/.config/nvim
+  ln -sf $PWD/.vim $HOME/.config/nvim
 elif [ ! -d $HOME/.vim ]; then
-  ln -sfr $PWD/.vim $HOME
+  ln -sf $PWD/.vim $HOME
 fi
 
 # Setup Command-T
