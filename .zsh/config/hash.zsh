5b2c6bd1b205ed4442230bb649143c47b9db307c
Z5483 <khuenguyrn123456@gmail.com>
2020-04-18 05:37:37 -0400

    refactor: remove hashed directories

--- .zsh/config/hash.zsh
+++ .zsh/config/hash.zsh
@@ -5,18 +5,10 @@
 # test -d $SOME_DIR && hash -d $SHORTCUT=$SOME_DIR
 
 # Work machine:
-local BIN=~/bin
-local CODE=~/code
-local COMPILER=~/.vim/after/compiler
-local DOTFILES=~/dotfiles
 local LINUX=/usr/src/linux
 local OVERLAY=/var/db/repos
 local PORTAGE=/etc/portage
 
-test -d "$BIN" && hash -d bin="$BIN"
-test -d "$CODE" && hash -d code="$CODE"
-test -d "$COMPILER" && hash -d compiler="$COMPILER"
-test -d "$DOTFILES" && hash -d dots="$DOTFILES"
 test -d "$LINUX" && hash -d linux="$LINUX"
 test -d "$OVERLAY" && hash -d overlay="$OVERLAY"
 test -d "$PORTAGE" && hash -d portage="$PORTAGE"
