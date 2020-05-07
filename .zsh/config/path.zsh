5c05ecfcea17db543608e675394a5531e7de652a
Z5483 <khuenguyrn123456@gmail.com>
2020-05-07 14:34:57 -0400

    feat(zsh): add more paths

--- .zsh/config/path.zsh
+++ .zsh/config/path.zsh
@@ -2,11 +2,17 @@ SYSTEM_PATH=$PATH
 unset PATH
 
 # keep these on separate lines to make changing their order easier
-PATH=$PATH:$HOME/.zsh/bin
 PATH=$PATH:$HOME/bin
+PATH=$PATH:$HOME/.zsh/bin
 PATH=$PATH:$HOME/.local/bin
 
 PATH=$PATH:$HOME/.gem/ruby/2.7.0/bin
+PATH=$PATH:$HOME/.cargo/bin
+
+if [[ -d /usr/lib/llvm/10/bin ]]; then
+        PATH=$PATH:/usr/lib/llvm/10/bin
+fi
+
 PATH=$PATH:/bin
 PATH=$PATH:/sbin
 PATH=$PATH:/usr/bin
