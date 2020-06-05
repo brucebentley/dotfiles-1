1d849a1816bf6c2a641947c22768273412f7b549
Z5483 <Z5483Y@gmail.com>
2020-06-05 15:14:01 -0400

    chore(zsh): remove aliases

--- .zsh/config/aliases.zsh
+++ .zsh/config/aliases.zsh
@@ -1,17 +1,12 @@
-alias ..='cd ..'
-alias ....='cd ../..'
 alias ......='cd ../../..'
-alias cp='cp -rfv'
-alias d='hash -d'
+alias ....='cd ../..'
+alias ..='cd ..'
 alias e=exit
 alias g=git
 alias l='ls --color=auto'
 alias ll='ls -alF --color=auto'
-alias mkdir='mkdir -pv'
-alias mv='mv -fuv'
 alias n=ncmpcpp
 alias r=vifm
-alias rm='rm -rfv'
 alias s=ssh
 alias t=tmux
 alias v=vim
