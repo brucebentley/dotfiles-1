5944f4d85e36f5c3733bdc2237f7b0218023a573
Z5483 <Z5483Y@gmail.com>
2020-06-13 03:38:13 -0400

    refactor(zsh): remove useless exported variables

--- .zsh/config/exports.zsh
+++ .zsh/config/exports.zsh
@@ -3,13 +3,7 @@ export CORRECT_IGNORE_FILE='.*'
 export FZF_DEFAULT_COMMAND="find -L"
 export FZF_DEFAULT_OPTS="--algo=v1 --color=16 --info=inline"
 
+export EDITOR=nvim
 export PAGER=less
 export LESS='-iFMRSX'
-
-if command -v nvim > /dev/null; then
-	export EDITOR=nvim
-	export MANPAGER='nvim +Man!'
-else
-	export EDITOR=vim
-	export MANPAGER='less'
-fi
+export MANPAGER='nvim +Man'
