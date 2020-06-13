8988417ef104e0dbe97c16b057699aac60a11301
Z5483 <Z5483Y@gmail.com>
2020-06-13 04:07:42 -0400

    refactor(zsh): merge git.zsh to prompt.zsh

--- .zsh/config/prompt.zsh
+++ .zsh/config/prompt.zsh
@@ -3,6 +3,23 @@ __EMANON[ITALIC_OFF]=$'\033[23m'
 
 DISABLE_UPDATE_PROMPT=true
 
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' stagedstr "%F{green}●%f"
+zstyle ':vcs_info:*' unstagedstr "%F{red}●%f"
+zstyle ':vcs_info:*' use-simple true
+zstyle ':vcs_info:git+set-message:*' hooks git-untracked
+zstyle ':vcs_info:git*:*' formats '[%b%m%c%u] '
+zstyle ':vcs_info:git*:*' actionformats '[%b|%a%m%c%u] '
+
+function +vi-git-untracked() {
+	emulate -L zsh
+
+	if [[ -n $(git ls-files --exclude-standard --others 2> /dev/null) ]]; then
+		hook_com[unstaged]+="%F{blue}●%f"
+	fi
+}
+
 RPROMPT_BASE="\${vcs_info_msg_0_}%F{blue}%~%f"
 SPROMPT="zsh: correct %F{red}'%R'%f to %F{red}'%r'%f [%B%Uy%u%bes, %B%Un%u%bo, %B%Ue%u%bdit, %B%Ua%u%bbort]? "
 
@@ -84,3 +101,18 @@ function -report-start-time() {
 	fi
 }
 add-zsh-hook precmd -report-start-time
+
+function -record-command() {
+	__EMANON[LAST_COMMAND]="$2"
+}
+add-zsh-hook preexec -record-command
+
+function -maybe-show-vcs-info() {
+	local LAST="$__EMANON[LAST_COMMAND]"
+
+	__EMANON[LAST_COMMAND]="<unset>"
+	if [[ $LAST[(w)1] =~ "cd|cp|rm|mv|touch|git" ]]; then
+		vcs_info
+	fi
+}
+add-zsh-hook precmd -maybe-show-vcs-info
