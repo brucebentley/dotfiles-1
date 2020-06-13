fb04d767f16ab496b6bed3ad636bbb80cfbd12e5
Z5483 <Z5483Y@gmail.com>
2020-06-13 03:53:35 -0400

    refactor(zsh): remove zstyle settings

--- .zsh/config/completion.zsh
+++ .zsh/config/completion.zsh
@@ -6,9 +6,7 @@ else
 	compinit -i -u
 fi
 
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' menu select
+
+zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format %F{default}%B%{$__EMANON[ITALIC_ON]%}--- %d ---%{$__EMANON[ITALIC_OFF]%}%b%f
-zstyle -e ':completion:*' special-dirs '[[ $PREFIX = (../)#(..) ]] && reply=(..)'
