c1f62c7f6a099f5f420b79e357ef79c0df5ae03d
Z5483 <Z5483Y@gmail.com>
2020-06-15 17:52:24 -0400

    revert(zsh): restore completion behaviors

--- .zsh/config/completion.zsh
+++ .zsh/config/completion.zsh
@@ -7,6 +7,7 @@ else
 fi
 
 zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format %F{default}%B%{$__EMANON[ITALIC_ON]%}--- %d ---%{$__EMANON[ITALIC_OFF]%}%b%f
