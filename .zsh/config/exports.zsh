3cf9ab5b7f672ebc677139b11e6f5d95f1feda7f
Z5483 <Z5483Y@gmail.com>
2020-06-16 09:15:15 -0400

    style: minor modification

--- .zsh/config/exports.zsh
+++ .zsh/config/exports.zsh
@@ -1,9 +1,12 @@
 export CORRECT_IGNORE_FILE='.*'
 
+export EDITOR=nvim
+
+export MANPAGER='nvim +Man!'
+
+export PAGER=less
+
 export FZF_DEFAULT_COMMAND="find -L"
 export FZF_DEFAULT_OPTS="--algo=v1 --color=16 --info=inline --no-bold"
 
-export EDITOR=nvim
-export PAGER=less
 export LESS='-iFMRSX'
-export MANPAGER='nvim +Man!'
