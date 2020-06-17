3e4b1ba775e452640e16b50ef7f1051dcdc41237
Z5483 <Z5483Y@gmail.com>
2020-06-17 06:18:30 -0400

    revert(zsh): set less as manpager

--- .zsh/config/exports.zsh
+++ .zsh/config/exports.zsh
@@ -2,8 +2,15 @@ export CORRECT_IGNORE_FILE='.*'
 
 export EDITOR=nvim
 
-export MANPAGER='nvim +Man!'
+export MANPAGER=less
 
 export PAGER=less
 
 export LESS='-iFMRSX'
+
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;38;5;208m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[04;38;5;111m'
