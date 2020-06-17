2918286539f3f54b136f089cee048f727a154181
Z5483 <Z5483Y@gmail.com>
2020-06-17 06:26:47 -0400

    feat(zsh): add zsh-history-substring-search plugin

--- .zsh/config/bindings.zsh
+++ .zsh/config/bindings.zsh
@@ -9,6 +9,13 @@ if tput cbt &> /dev/null; then
 	bindkey "$(tput cbt)" reverse-menu-complete
 fi
 
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey '^K' history-substring-search-up
+bindkey '^J' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
 function zle-keymap-select zle-line-init {
 	if [[ $KEYMAP == main ]]; then
 		printf $__EMANON[LINE_CURSOR]
--- .zshrc
+++ .zshrc
@@ -22,5 +22,6 @@ source $HOME/.zsh/config/wrappers.zsh
 source $HOME/.zsh/config/path.zsh
 source $HOME/.zsh/config/prompt.zsh
 
+source $HOME/.zsh/plugin/zsh-history-substring-search/zsh-history-substring-search.zsh
 source $HOME/.zsh/plugin/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $HOME/.zsh/plugin/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
