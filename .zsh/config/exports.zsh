88b2141c4e973ec8ad40edadd06db82bef616b40
Z5483 <Z5483Y@gmail.com>
2020-06-16 20:15:58 -0400

    chore(zsh): cleaning

--- .zsh/config/exports.zsh
+++ .zsh/config/exports.zsh
@@ -6,7 +6,4 @@ export MANPAGER='nvim +Man!'
 
 export PAGER=less
 
-export FZF_DEFAULT_COMMAND="find -L"
-export FZF_DEFAULT_OPTS="--algo=v1 --color=16 --info=inline --no-bold"
-
 export LESS='-iFMRSX'
