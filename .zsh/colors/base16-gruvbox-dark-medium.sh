4aafb0e754d48194a5713daaf084bf42d560e968
Z5483 <Z5483Y@gmail.com>
2020-06-23 00:25:56 -0400

    style: re-indent files

--- .zsh/colors/base16-gruvbox-dark-medium.sh
+++ .zsh/colors/base16-gruvbox-dark-medium.sh
@@ -29,24 +29,24 @@ color_foreground="d5/c4/a1" # Base 05
 color_background="28/28/28" # Base 00
 
 if [ -n "$TMUX" ]; then
-  # Tell tmux to pass the escape sequences through
-  # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
-  put_template() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' $@; }
-  put_template_var() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' $@; }
-  put_template_custom() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' $@; }
+	# Tell tmux to pass the escape sequences through
+	# (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+	put_template() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' $@; }
+	put_template_var() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' $@; }
+	put_template_custom() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' $@; }
 elif [ "${TERM%%[-.]*}" = "screen" ]; then
-  # GNU screen (screen, screen-256color, screen-256color-bce)
-  put_template() { printf '\033P\033]4;%d;rgb:%s\007\033\\' $@; }
-  put_template_var() { printf '\033P\033]%d;rgb:%s\007\033\\' $@; }
-  put_template_custom() { printf '\033P\033]%s%s\007\033\\' $@; }
+	# GNU screen (screen, screen-256color, screen-256color-bce)
+	put_template() { printf '\033P\033]4;%d;rgb:%s\007\033\\' $@; }
+	put_template_var() { printf '\033P\033]%d;rgb:%s\007\033\\' $@; }
+	put_template_custom() { printf '\033P\033]%s%s\007\033\\' $@; }
 elif [ "${TERM%%-*}" = "linux" ]; then
-  put_template() { [ $1 -lt 16 ] && printf "\e]P%x%s" $1 $(echo $2 | sed 's/\///g'); }
-  put_template_var() { true; }
-  put_template_custom() { true; }
+	put_template() { [ $1 -lt 16 ] && printf "\e]P%x%s" $1 $(echo $2 | sed 's/\///g'); }
+	put_template_var() { true; }
+	put_template_custom() { true; }
 else
-  put_template() { printf '\033]4;%d;rgb:%s\033\\' $@; }
-  put_template_var() { printf '\033]%d;rgb:%s\033\\' $@; }
-  put_template_custom() { printf '\033]%s%s\033\\' $@; }
+	put_template() { printf '\033]4;%d;rgb:%s\033\\' $@; }
+	put_template_var() { printf '\033]%d;rgb:%s\033\\' $@; }
+	put_template_custom() { printf '\033]%s%s\033\\' $@; }
 fi
 
 # 16 color space
@@ -77,23 +77,23 @@ put_template 21 $color21
 
 # foreground / background / cursor color
 if [ -n "$ITERM_SESSION_ID" ]; then
-  # iTerm2 proprietary escape codes
-  put_template_custom Pg d5c4a1 # foreground
-  put_template_custom Ph 282828 # background
-  put_template_custom Pi d5c4a1 # bold color
-  put_template_custom Pj 504945 # selection color
-  put_template_custom Pk d5c4a1 # selected text color
-  put_template_custom Pl d5c4a1 # cursor
-  put_template_custom Pm 282828 # cursor text
+	# iTerm2 proprietary escape codes
+	put_template_custom Pg d5c4a1 # foreground
+	put_template_custom Ph 282828 # background
+	put_template_custom Pi d5c4a1 # bold color
+	put_template_custom Pj 504945 # selection color
+	put_template_custom Pk d5c4a1 # selected text color
+	put_template_custom Pl d5c4a1 # cursor
+	put_template_custom Pm 282828 # cursor text
 else
-  put_template_var 10 $color_foreground
-  if [ "$BASE16_SHELL_SET_BACKGROUND" != false ]; then
-    put_template_var 11 $color_background
-    if [ "${TERM%%-*}" = "rxvt" ]; then
-      put_template_var 708 $color_background # internal border (rxvt)
-    fi
-  fi
-  put_template_custom 12 ";7" # cursor (reverse video)
+	put_template_var 10 $color_foreground
+	if [ "$BASE16_SHELL_SET_BACKGROUND" != false ]; then
+		put_template_var 11 $color_background
+		if [ "${TERM%%-*}" = "rxvt" ]; then
+			put_template_var 708 $color_background # internal border (rxvt)
+		fi
+	fi
+	put_template_custom 12 ";7" # cursor (reverse video)
 fi
 
 # clean up
