3703f12759fb8a4a4b5c4d2897763e4d47836fbe
Z5483 <Z5483Y@gmail.com>
2020-07-05 11:32:04 -0400

    feat(hash): add more directories and rearrange

--- .zsh/config/hash.zsh
+++ .zsh/config/hash.zsh
@@ -1,13 +1,17 @@
 #!/usr/bin/env zsh
 
-local LOG=/var/log
-local PORTAGE=/etc/portage
-local LINUX=/usr/src/linux
-local OVERLAY=/var/db/repos
 local CODE=~/Code
+local CONSOLEFONT=/usr/share/consolefonts
+local FONT=/usr/share/fonts
+local LINUX=/usr/src/linux
+local LOG=/var/log
+local OVERLAY=/var/db/repos
+local PORTAGE=/etc/portage
 
-test -d "$LOG" && hash -d log="$LOG"
-test -d "$PORTAGE" && hash -d portage="$PORTAGE"
-test -d "$LINUX" && hash -d linux="$LINUX"
-test -d "$OVERLAY" && hash -d overlay="$OVERLAY"
 test -d "$CODE" && hash -d code="$CODE"
+test -d "$CONSOLEFONT" && hash -d consolefont="$CONSOLEFONT"
+test -d "$FONT" && hash -d font="$FONT"
+test -d "$LINUX" && hash -d linux="$LINUX"
+test -d "$LOG" && hash -d log="$LOG"
+test -d "$OVERLAY" && hash -d overlay="$OVERLAY"
+test -d "$PORTAGE" && hash -d portage="$PORTAGE"
