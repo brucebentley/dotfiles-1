73bee40d2da5c694f8752c363ad84b73a13bf0d0
Z5483 <Z5483Y@gmail.com>
2020-07-05 12:02:21 -0400

    feat(less): disable history

--- .zsh/config/exports.zsh
+++ .zsh/config/exports.zsh
@@ -36,6 +36,8 @@ export PAGER=less
 
 export LESS='-iFMRSX'
 
+export LESSHISTFILE=/dev/null
+
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;38;5;208m'
 export LESS_TERMCAP_me=$'\E[0m'
